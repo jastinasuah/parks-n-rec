d37031daa398e29e06d3d51ca0cae83dcdcfbb0a
jastinasuah <jastinasuah@gmail.com>
2022-06-22 15:15:58 +0800

    tried other code

--- server.js
+++ server.js
@@ -1,3 +1,41 @@
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const PORT = 8000
+
+// app.use(cors())
+
+// const pokemon = {
+//     'pikachu':{
+//         'type':'electric',
+//         'next Evolution': 'Raichu'
+//     },
+//     'charmander':{
+//         'type':'fire',
+//         'next Evolution': 'Charmeleon'
+//     },
+//     'ditto':{
+//         'type':'normal',
+//         'next Evolution': 'Does not Evolve'
+//     }
+// }
+
+// app.get('/',(request,response)=>{
+//     response.sendFile(__dirname+'/index.html')
+// })
+
+// app.get('/api:pokemonName',(request,response)=>{
+//     const pokemonName = request.params.pokemonName.toLowerCase()
+//     if (pokemon[pokemonName]){
+//         response.json(pokemon[pokemonName])
+//     }
+//     else {response.json (pokemon['ditto'])}
+// })
+
+// app.listen(process.env.PORT || PORT, ()=>{
+//     console.log(`The server is running on port ${PORT}! You better go catch it!`)
+// })
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -5,31 +43,35 @@ const PORT = 8000
 
 app.use(cors())
 
-const pokemon = {
-    'pikachu':{
-        'type':'electric',
-        'next Evolution': 'Raichu'
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName':'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England' 
     },
-    'charmander':{
-        'type':'fire',
-        'next Evolution': 'Charmeleon'
+    'chance the rapper':{
+        'age': 29,
+        'birthName':'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois' 
     },
-    'ditto':{
-        'type':'normal',
-        'next Evolution': 'Does not Evolve'
+    'dylan':{
+        'age': 29,
+        'birthName':'Dylan',
+        'birthLocation': 'Dylan' 
     }
 }
 
-app.get('/',(request,response)=>{
-    response.sendFile(__dirname+'/index.html')
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api:pokemonName',(request,response)=>{
-    const pokemonName = request.params.pokemonName.toLowerCase()
-    if (pokemon[pokemonName]){
-        response.json(pokemon[pokemonName])
+app.get('/api/:rapperName', (request,response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers['dylan'])
     }
-    else {response.json (pokemon['ditto'])}
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
