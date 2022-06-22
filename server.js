0b68521345eb9aef74057370f84cbac8377d91d3
jastinasuah <jastinasuah@gmail.com>
2022-06-22 20:56:19 +0800

    rappers

--- server.js
+++ server.js
@@ -1,41 +1,3 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const PORT = 8000
-
-app.use(cors())
-
-const pokemon = {
-    'pikachu':{
-        'type':'electric',
-        'next Evolution': 'Raichu'
-    },
-    'charmander':{
-        'type':'fire',
-        'next Evolution': 'Charmeleon'
-    },
-    'ditto':{
-        'type':'normal',
-        'next Evolution': 'Does not Evolve'
-    }
-}
-
-app.get('/',(request,response)=>{
-    response.sendFile(__dirname+'/index.html')
-})
-
-app.get('/api:pokemonName',(request,response)=>{
-    const pokemonName = request.params.pokemonName.toLowerCase()
-    if (pokemon[pokemonName]){
-        response.json(pokemon[pokemonName])
-    }
-    else {response.json (pokemon['ditto'])}
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`The server is running on port ${PORT}! You better go catch it!`)
-})
-
 // const express = require('express')
 // const app = express()
 // const cors = require('cors')
@@ -43,37 +5,75 @@ app.listen(process.env.PORT || PORT, ()=>{
 
 // app.use(cors())
 
-// const rappers = {
-//     '21 savage':{
-//         'age': 29,
-//         'birthName':'Shéyaa Bin Abraham-Joseph',
-//         'birthLocation': 'London, England' 
+// const pokemon = {
+//     'pikachu':{
+//         'type':'electric',
+//         'next Evolution': 'Raichu'
 //     },
-//     'chance the rapper':{
-//         'age': 29,
-//         'birthName':'Chancelor Bennett',
-//         'birthLocation': 'Chicago, Illinois' 
+//     'charmander':{
+//         'type':'fire',
+//         'next Evolution': 'Charmeleon'
 //     },
-//     'dylan':{
-//         'age': 29,
-//         'birthName':'Dylan',
-//         'birthLocation': 'Dylan' 
+//     'ditto':{
+//         'type':'normal',
+//         'next Evolution': 'Does not Evolve'
 //     }
 // }
 
-// app.get('/', (request, response)=>{
-//     response.sendFile(__dirname + '/index.html')
+// app.get('/',(request,response)=>{
+//     response.sendFile(__dirname+'/index.html')
 // })
 
-// app.get('/api/:rapperName', (request,response)=>{
-//     const rappersName = request.params.rapperName.toLowerCase()
-//     if(rappers[rappersName]){
-//         response.json(rappers[rappersName])
-//     }else{
-//         response.json(rappers['dylan'])
+// app.get('/api:pokemonName',(request,response)=>{
+//     const pokemonName = request.params.pokemonName.toLowerCase()
+//     if (pokemon[pokemonName]){
+//         response.json(pokemon[pokemonName])
 //     }
+//     else {response.json (pokemon['ditto'])}
 // })
 
 // app.listen(process.env.PORT || PORT, ()=>{
 //     console.log(`The server is running on port ${PORT}! You better go catch it!`)
 // })
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName':'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England' 
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName':'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois' 
+    },
+    'dylan':{
+        'age': 29,
+        'birthName':'Dylan',
+        'birthLocation': 'Dylan' 
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request,response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
